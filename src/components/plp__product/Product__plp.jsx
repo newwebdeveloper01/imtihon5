de2685b78141cdc5b4631214faf585932c093ef3
Akbar Nurullayev <108784881+Nurullayev-Akbar@users.noreply.github.com>
2023-03-01 20:34:37 +0500

    imtihon 5 oy

--- src/components/plp__product/Product__plp.jsx
+++ src/components/plp__product/Product__plp.jsx
@@ -4,7 +4,7 @@ import { Container } from '../../utils/Container';
 import Product from "../project/ProductPLP.jsx"
 import "./Product__plp.scss"
 const Product__plp = () => {
-    const [data , isLoding]=useFetchData("/products?offset=1&limit=20")
+    const [data , isLoding]=useFetchData("/products?offset=1&limit=20");
   return (
     <section className="main__products">
         <Container>
