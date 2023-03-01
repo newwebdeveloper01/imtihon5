b6d657ee51944e09af7ca5bd9cf6e597e9e666d6
Akbar Nurullayev <108784881+Nurullayev-Akbar@users.noreply.github.com>
2023-03-01 20:46:11 +0500

    imtihon 5 oy

--- src/components/plpproduct/Productplp.jsx
+++ src/components/plpproduct/Productplp.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import useFetchData from '../../hooks/useFetchData'
 import { Container } from '../../utils/Container';
 import Product from "../project/ProductPLP.jsx"
-import "./Product__plp.scss"
+import "./Productplp.scss"
 const Product__plp = () => {
     const [data , isLoding]=useFetchData("/products?offset=1&limit=20");
   return (
--- src/routes/plp/Plp.jsx
+++ src/routes/plp/Plp.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import CategoryList from '../../components/category_list/CategoryList'
 import Search from '../../components/search/Search';
-import Product__plp from "../../components/Plp__product/Product__plp";
+import Product__plp from "../../components/plpproduct/Productplp";
 import Baner from '../../components/plp__baner/Baner';
 
 const Plp = () => {
