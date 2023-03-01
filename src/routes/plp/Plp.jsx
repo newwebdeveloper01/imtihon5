1167e8eccaada3c419d44170a653b052e45e0f09
Akbar Nurullayev <108784881+Nurullayev-Akbar@users.noreply.github.com>
2023-03-01 19:58:36 +0500

    imtihon 5 oy

--- src/routes/plp/Plp.jsx
+++ src/routes/plp/Plp.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import CategoryList from '../../components/category_list/CategoryList'
 import Search from '../../components/search/Search';
-import Product__plp from "../../components/Plp__product/Product__plp.jsx";
+import Product__plp from "../../components/Plp__product/Product__plp";
 import Baner from '../../components/plp__baner/Baner';
 
 const Plp = () => {
